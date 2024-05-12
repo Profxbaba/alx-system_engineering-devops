a4f4ffc246a6c9983249761c044d676ed5ca56dd
Oluwashina Olaniyan <daddyslove40@gmail.com>
2024-05-11 18:25:29 -0700

    Web stack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,29 +1,6 @@
-# Puppet manifest to fix Apache returning a 500 error using strace
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-# Install strace package
-package { 'strace':
-  ensure => installed,
-}
-
-# Run strace on Apache process to find the issue
-exec { 'run_strace_on_apache':
-  command     => 'strace -o /tmp/strace_output.txt -p $(pgrep apache2)',
-  path        => '/bin:/usr/bin',
-  refreshonly => true,
-  subscribe   => Service['apache2'],
-  notify      => Service['apache2'],
-}
-
-# Fix the issue found by strace
-file_line { 'increase_max_connections':
-  path    => '/etc/apache2/apache2.conf',
-  line    => 'MaxConnectionsPerChild 100',
-  match   => '^MaxConnectionsPerChild',
-  notify  => Service['apache2'],
-  require => Exec['run_strace_on_apache'],
-}
-
-# Ensure Apache service is running
-service { 'apache2':
-  ensure => running,
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
